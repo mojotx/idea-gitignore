f92e0d21d52b6ac488918521738f73eb73975dd0
hsz <jakub@chrzanowski.info>
2019-07-30 09:12:21 +0200

    merge with master

--- src/mobi/hsz/idea/gitignore/IgnoreBundle.java
+++ src/mobi/hsz/idea/gitignore/IgnoreBundle.java
@@ -99,12 +99,7 @@ public class IgnoreBundle {
     ));
 
     /**Highlighting for the mentioned languages already provided by IDEA core**/
-    public static final IgnoreLanguage[] IGNORE_LANGUAGES_HIGHLIGHTING_EXCLUDED = {
-            GitLanguage.INSTANCE,
-            GitExcludeLanguage.INSTANCE,
-            MercurialLanguage.INSTANCE,
-            PerforceLanguage.INSTANCE
-    };
+    public static final IgnoreLanguage[] IGNORE_LANGUAGES_HIGHLIGHTING_EXCLUDED = {};
 
     /** Available IgnoreFileType instances filtered with {@link IgnoreLanguage#isVCS()} condition. */
     public static final IgnoreLanguages VCS_LANGUAGES = new IgnoreLanguages(
